ca9927bf029fff06d2417115c14438a4aaa33fc3
Darren Cauthon <darren@cauthon.com>
2023-03-20 11:43:12 -0500

    More test cases.

--- lib/drill_knight/remove_string_spaces.ex
+++ lib/drill_knight/remove_string_spaces.ex
@@ -14,6 +14,15 @@ defmodule DrillKnight.RemoveStringSpaces do
       iex> DrillKnight.RemoveStringSpaces.no_space("8 j 8   mBliB8g  imjB8B8  jl  B")
       "8j8mBliB8gimjB8B8jlB"
 
+      iex> DrillKnight.RemoveStringSpaces.no_space("8 j 8   mBliB8g  imjB8B8  jl  B")
+      "8j8mBliB8gimjB8B8jlB"
+
+      iex> DrillKnight.RemoveStringSpaces.no_space("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd")
+      "88Bifk8hB8BB8BBBB888chl8BhBfd"
+
+      iex> DrillKnight.RemoveStringSpaces.no_space("8aaaaa dddd r     ")
+      "8aaaaaddddr"
+
   """
   def no_space(x) do
     String.replace(x, " ", "")
