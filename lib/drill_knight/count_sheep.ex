f72256f60c4558c38fa659aa18bb6c7ed5bcaa4e
Darren Cauthon <darren@cauthon.com>
2023-03-20 18:25:35 -0500

    First passing test.

---
+++ lib/drill_knight/count_sheep.ex
@@ -0,0 +1,18 @@
+defmodule DrillKnight.CountSheep do
+  @moduledoc """
+  Documentation for `DrillKnight.CountSheep`.
+  """
+
+  @doc """
+  String repeat.
+
+  ## Examples
+
+      iex> DrillKnight.CountSheep.count([])
+      0
+
+  """
+  def count(sheep) do
+    0
+  end
+end
