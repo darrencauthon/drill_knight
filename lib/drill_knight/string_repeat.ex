db270d59933d1bbabeb01de390ce95b963463340
Darren Cauthon <darren@cauthon.com>
2023-03-20 18:21:27 -0500

    String repeat (#1)

---
+++ lib/drill_knight/string_repeat.ex
@@ -0,0 +1,27 @@
+defmodule DrillKnight.StringRepeat do
+  @moduledoc """
+  Documentation for `DrillKnight.StringRepeat`.
+  """
+
+  @doc """
+  String repeat.
+
+  ## Examples
+
+      iex> DrillKnight.StringRepeat.repeat(0, "")
+      ""
+
+      iex> DrillKnight.StringRepeat.repeat(1, "x")
+      "x"
+
+      iex> DrillKnight.StringRepeat.repeat(2, "x")
+      "xx"
+
+      iex> DrillKnight.StringRepeat.repeat(5, "Hello")
+      "HelloHelloHelloHelloHello"
+
+  """
+  def repeat(count, string) do
+     (0..count - 1) |> Enum.map_join(fn _ -> string end)
+  end
+end
