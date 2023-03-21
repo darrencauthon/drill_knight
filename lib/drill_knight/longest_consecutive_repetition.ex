9cf29a7cd4b6fe31a9f2b6be8ec83292810d9c60
Darren Cauthon <darren@cauthon.com>
2023-03-21 10:53:26 -0500

    Refactor.

--- lib/drill_knight/longest_consecutive_repetition.ex
+++ lib/drill_knight/longest_consecutive_repetition.ex
@@ -42,18 +42,10 @@ defmodule DrillKnight.LongestConsecutiveRepetition do
   def longest(string) when string == "", do: {"", 0}
 
   def longest(string) do
-    chars = String.codepoints(string)
-
-    next(tl(chars), {hd(chars), 1}, {hd(chars), 1})
+    string |> String.codepoints
+           |> Enum.chunk_by(&(&1))
+           |> Enum.map(fn x -> {List.first(x), length(x)} end)
+           |> Enum.max_by(fn {_x, y} -> y end)
   end
 
-  def next(chars, _, best) when chars == [], do: best
-
-  def next(chars, last, best) when hd(chars) == elem(last, 0) do
-    last = {elem(last, 0), elem(last, 1) + 1}
-    best = if elem(last, 1) > elem(best, 1), do: last, else: best
-    next(tl(chars), last, best)
-  end
-
-  def next(chars, _, best), do: next(tl(chars), {hd(chars), 1}, best)
 end
