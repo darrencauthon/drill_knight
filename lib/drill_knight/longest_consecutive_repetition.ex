0ced398996130828b9e44c0699bee6981dd0be74
Darren Cauthon <darren@cauthon.com>
2023-03-21 07:07:29 -0500

    First passing test.

---
+++ lib/drill_knight/longest_consecutive_repetition.ex
@@ -0,0 +1,16 @@
+defmodule DrillKnight.LongestConsecutiveRepetition do
+  @moduledoc """
+  Documentation for `DrillKnight.LongestConsecutiveRepetition`.
+  """
+
+  @doc """
+  String repeat.
+
+  ## Examples
+
+      iex> DrillKnight.LongestConsecutiveRepetition.longest('')
+      {"", 0}
+
+  """
+  def longest(_string), do: {"", 0}
+end
---
+++ test/drill_knight/longest_consecutive_repetition_test.exs
@@ -0,0 +1,8 @@
+defmodule DrillKnight.LongestConsecutiveRepetitionTest do
+  @moduledoc """
+  Tests for `DrillKnight.LongestConsecutiveRepetition`.
+  """
+
+  use ExUnit.Case
+  doctest DrillKnight.LongestConsecutiveRepetition
+end
